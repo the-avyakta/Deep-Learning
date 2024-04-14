4d0e90b8da6bd3b89ee356e6385c9f840ceca60b
Aniket Mishra <info@bizoally.com>
2024-04-14 20:57:56 +0530

    cnn

---
+++ cnn.py
@@ -0,0 +1,37 @@
+from keras import layers, models
+from keras import datasets
+
+custom_file=datasets.mnist #10
+choose_dataset=int(input("do you want to choose dataset? 0 for no, 1 for yes: "))
+if (choose_dataset==0):
+    dataset = datasets.mnist
+else:
+    dataset=custom_file
+output_classes = int(input("Enter Number of classes: "))
+
+(x_train, y_train), (x_test, y_test) = dataset.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0 
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'), 
+    layers.Dense(output_classes, activation='softmax')  
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print('Test accuracy:', test_acc)
